485a2bd0ed28f3abe7f61001dfe4556d6ba370a2
mvaradi <mvaradi@ebi.ac.uk>
2021-02-11 15:58:05 +0000

    Refactoring

--- src/app/json.pipe.ts
+++ src/app/json.pipe.ts
@@ -1,12 +1,18 @@
-import { Pipe, PipeTransform } from "@angular/core";
+import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({name: 'keysPipe'})
 export class KeysPipe implements PipeTransform {
-  transform(value, args:string[]) : any {
-    let keys = [];
-    for (let key in value) {
-      keys.push({key: key, value: value[key]});
+    transform(value, args: string[]): any {
+        const keysAndValues = [];
+        if (!value) {
+            return keysAndValues;
+        }
+        const keyItems = Object.keys(value);
+        if (keyItems) {
+            for (let i = 0; i < keyItems.length; i++) {
+                keysAndValues.push({key: keyItems[i], value: value[keyItems[i]]});
+            }
+        }
+        return keysAndValues;
     }
-    return keys;
-  }
 }
