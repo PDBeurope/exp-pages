7ebd2c2a6c01e00063ba318d82f341fa0d9f5a83
mvaradi <mvaradi@ebi.ac.uk>
2021-02-11 15:45:50 +0000

    Removing obsolete import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import {NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
-import {PolymerElement} from '@vaadin/angular2-polymer';
 
 import {AppComponent} from './app.component';
 import {PdbService} from './app.service';
